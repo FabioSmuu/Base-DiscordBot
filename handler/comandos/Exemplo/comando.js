d77783683162765c292f726ffe8fa529fc6ea2a0
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2020-06-26 07:23:04 -0300

    Update comando.js

--- handler/comandos/Exemplo/comando.js
+++ handler/comandos/Exemplo/comando.js
@@ -5,5 +5,5 @@ module.exports.info = {
 }
 
 module.exports.comando = (client, msg, args) => {
-    //Aqui é onde ocorre a magina, pois tudo o que for criado neste bloco será executado ao chamar o comando.
+    //Aqui é onde ocorre a magica, pois, tudo o que for criado neste bloco será executado ao chamar o comando.
 }
