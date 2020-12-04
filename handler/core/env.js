103a3f67a1237092513194c301319e391c6435a7
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2020-12-04 13:27:01 -0300

    Update env.js

--- handler/core/env.js
+++ handler/core/env.js
@@ -6,23 +6,16 @@ path = require('path')
 let env = {},
 file = fs.readFileSync(path.resolve(process.cwd(), '.env'), 'utf8')
 
-file.toString().split(/\n|\r|\r\n/).forEach(async (line) => {
-	let valor = line.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
+file.toString().split(/\n|\r|\r\n/).map(async (linha) => {
+	let valor = linha.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
 	if (valor != null) {
-	  let val = (valor[2] || ''),
-	  end = val.length - 1
-
-	  if (val[0] === '"' && val[end] === '"') {
-		val = val.substring(1, end)
-		val = val.replace(/\\n/g, '\n')
-		
-	  } else val = val.trim()
-	  
-	  env[valor[1]] = val
+		let val = valor[2] || '',
+		end = val.length - 1
+		env[valor[1]] = (val[0] === '"' && val[end] === '"') ? val.substring(1, end).replace(/\\n/g, '\n') : val.trim()
 	}
 })
 
-Object.keys(env).forEach(async (key) => {
+Object.keys(env).map(async (key) => {
 	let retorno = (!Object.prototype.hasOwnProperty.call(process.env, key)) ? process.env[key] = env[key] : false
 	if (!retorno) console.log('[ENV] Erro na importação de valores.')
 })
