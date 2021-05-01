bf991b7ec6f56630112e6065c836826521c02778
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2021-04-30 21:20:42 -0300

    Update comando.js

--- handler/comandos/Exemplo/comando.js
+++ handler/comandos/Exemplo/comando.js
@@ -2,8 +2,8 @@ module.exports.info = {
     cmd: ['exemplo'/*, 'Insira valores para usar como comando.'*/],
     roles: [/*'Coloque aqui o nome das roles configurada na config.json', 'perm-bot', 'exemplo1', 'exemplo2'*/],
     users: [/*'Coloque aqui o nome dos users configurado na config.json', 'Fabio', 'exemplo1', 'exemplo2'*/],
-	deleted: true, //true para comando que se pagam sozinhos.
-	dm: false //true para comandos que funcionam em dm (conversa privada).
+    deleted: true, //true para comando que se pagam sozinhos.
+    dm: false //true para comandos que funcionam em dm (conversa privada).
 }
 
 module.exports.comando = (client, msg, args) => {
