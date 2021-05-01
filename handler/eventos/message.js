1a19cb9d252ed45b65a5f7be035e49b76dbbfd8b
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2021-04-30 21:21:31 -0300

    Update message.js

--- handler/eventos/message.js
+++ handler/eventos/message.js
@@ -1,25 +1,25 @@
 module.exports = async (client, msg) => {
     if (['discord.gg/', 'discordapp.com/invite/', 'invite.gg/', 'discord.io/', 'discord.me/', 'discord.plus/', 'dis.gd/'].some(invite => msg.content.includes(invite) && !msg.content.includes('https://discord.gg/bzjJBsS'))) {
-		msg.delete().then(msg.channel.send(`${msg.author} você não pode enviar links de outros servidores aqui!`).then(msg => msg.delete(5000)))
+        msg.delete().then(msg.channel.send(`${msg.author} você não pode enviar links de outros servidores aqui!`).then(msg => msg.delete(5000)))
     }
 
     let args = msg.content.slice(config.Prefixo.length).trim().split(/ +/g)
     let cmd = client.commands.get(args.shift().toLowerCase())
 
     if (
-		!cmd
-		|| msg.author.bot
-		|| msg.channel.type == 'dm'
-		&& msg.author.id != client.user.id
-		|| !msg.content.startsWith(config.Prefixo)
+        !cmd
+        || msg.author.bot
+        || (msg.channel.type == 'dm' && !cmd.info.dm)
+        || !msg.content.startsWith(config.Prefixo)
     )
 	return
 
     if (
-		msg.author.id == msg.channel.guild.ownerID
-		|| (cmd.info.users == 0 && cmd.info.roles == 0)
+		||*/ (cmd.info.users == 0 && cmd.info.roles == 0)
 		|| cmd.info.roles.some(r => msg.member.roles.some(b => b.id == config.roles[r]))
 		|| cmd.info.users.some(user => msg.author.id === config.users[user])
     )
-	return msg.delete(1), cmd.comando(client, msg, args)
+	
+	if (cmd.info.deleted) msg.delete(1)
+	return cmd.comando(client, msg, args)
 }
