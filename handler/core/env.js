5394c70eca83883c660789a0d586a7550adb6813
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2021-07-09 16:10:47 -0300

    Update env.js

--- handler/core/env.js
+++ handler/core/env.js
@@ -2,28 +2,28 @@ if (!config.envfake) return
 
 const fs = require('fs'), path = require('path')
 
-const removeAspas = string => string.replace(/['"](\w*)['"]/g, '$1').trim().replace(/\\n/g, '\n')
+const filterLine = string => string.replace(/['"](\w*)['"]/g, '$1').trim().replace(/\\n/g, '\n')
 
-const registrar = (chave, valor) => {
-	if (!Object.prototype.hasOwnProperty.call(process.env, chave)) {
-		process.env[chave] = valor
-		console.log(`[ENV] (${chave}) Carregada!`)
-	} else
-		console.log(`[ENV] Erro ao registrar (${chave}) Acesso negado.`)
-}
-
-const formatar = linha => {
-	const estrutura = linha.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
-
-	if (estrutura != null) {
-		const [undefined, chave, valor] = estrutura
-		const retorno = removeAspas(valor)
-
-		if (retorno) registrar(chave, retorno)
+const registerKey = (key, value) => {
+	if (Object.prototype.hasOwnProperty.call(process.env, key))
+		console.log(`[ENV] Erro ao registrar (${key}) Acesso negado.`)
+	else {
+		process.env[key] = filterLine(value)
+		console.log(`[ENV] (${key}) Carregada!`)
 	}
 }
 
+const fileToEnv = line => {
+	const newline = line.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
+	const [undefined, key, value] = newline || []
+
+	if (newline) registerKey(key, value)
+}
+
+const parseEnv = file => file.toString().split(/\n|\r|\r\n/).forEach(fileToEnv)
+
 const file = fs.readFileSync(path.resolve(process.cwd(), '.env'), 'utf8')
-file.toString().split(/\n|\r|\r\n/).forEach(formatar)
+parseEnv(file)
 
 //console.log(process.env)
+
