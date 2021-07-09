454286dfc44ddefe5b1499f0dc4a5dab8fab5d28
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2021-07-08 22:50:59 -0300

    Update env.js

--- handler/core/env.js
+++ handler/core/env.js
@@ -2,13 +2,9 @@ if (!config.envfake) return
 
 const fs = require('fs'), path = require('path')
 
-const removeAspas = string => {
-	return (string[0] === '"' && string[string.length-1] === '"')
-	? string.substring(1, string.length-1).replace(/\\n/g, '\n')
-	: string.trim()
-}
+const removeAspas = string => string.replace(/['"](\w*)['"]/g, '$1').trim().replace(/\\n/g, '\n')
 
-const registrarEnv = (chave, valor) => {
+const registrar = (chave, valor) => {
 	if (!Object.prototype.hasOwnProperty.call(process.env, chave)) {
 		process.env[chave] = valor
 		console.log(`[ENV] (${chave}) Carregada!`)
@@ -16,16 +12,18 @@ const registrarEnv = (chave, valor) => {
 		console.log(`[ENV] Erro ao registrar (${chave}) Acesso negado.`)
 }
 
-const formatarLinha = linha => {
+const formatar = linha => {
 	const estrutura = linha.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
 
 	if (estrutura != null) {
-		const [str, chave, valor] = estrutura
+		const [undefined, chave, valor] = estrutura
 		const retorno = removeAspas(valor)
 
-		if (retorno) registrarEnv(chave, retorno)
+		if (retorno) registrar(chave, retorno)
 	}
 }
 
 const file = fs.readFileSync(path.resolve(process.cwd(), '.env'), 'utf8')
-file.toString().split(/\n|\r|\r\n/).forEach(formatarLinha)
+file.toString().split(/\n|\r|\r\n/).forEach(formatar)
+
+//console.log(process.env)
