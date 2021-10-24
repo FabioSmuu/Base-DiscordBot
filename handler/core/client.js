f171cf6b6ffef2eb26123dbde11e7445be7cfebf
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2021-10-24 09:03:23 -0300

    Update client.js

--- handler/core/client.js
+++ handler/core/client.js
@@ -1,37 +1,52 @@
-const fs = require('fs')
-, util = require('util')
-, path = require('path')
-, Discord = {Client, Collection} = require('discord.js')
-, client = new Client()
+const { readdirSync, statSync, lstatSync } = require('fs')
+const { resolve, basename } = require('path')
+const Discord = {Client, Collection} = require('discord.js')
+const client = new Client()
 
 client.commands = new Collection()
 
 ////cmd
-function readDir(dir){
-	const files = {}
+console.log('\n[Commands]')
+const readFile = (dir, file) => {
+	if (!file.endsWith('.js')) return
+	const comandos = require(resolve(dir, file))
+	comandos.info.cmd.map(e => client.commands.set(e, comandos))
 
-	fs.readdirSync(dir)
-	.sort((a, b) => fs.statSync(path.resolve(dir, a)).mtime.getTime() - fs.statSync(path.resolve(dir, b)).mtime.getTime())
-	.map(file => {
-		if (fs.lstatSync(path.resolve(dir, file)).isFile()) {
-			if (file.endsWith('.js')) {
-				console.log(`- ${file} OK!`)
-				const comandos = require(path.resolve(dir, file))
-				comandos.info.cmd.map(e => client.commands.set(e, comandos))
-			}
-		}
-		else if (fs.lstatSync(path.resolve(dir, file)).isDirectory()) files[file] = readDir(path.resolve(dir, file))
-	})
+	console.log(`- ${file} OK!`)
 }
 
-readDir(path.resolve(__dirname, '../comandos/'))
+const mimeType = (dir, sub) => statSync(resolve(dir, sub)).mtime.getTime()
+const getStat = (dir, sub) => lstatSync(resolve(dir, sub))
+const readDir = dir => {
+	const files = {}
+
+	const getCommand = path => {
+		const stat = getStat(dir, path)
+		if (stat.isFile()) return readFile(dir, path)
+		if (stat.isDirectory()) return files[path] = readDir(resolve(dir, path))
+	}
+
+	readdirSync(dir)
+	.sort((path, name) => mimeType(dir, path) - mimeType(dir, name))
+	.forEach(getCommand)
+}
+
+readDir(resolve(__dirname, '../comandos/'))
 ////cmd
 
 ////evt
-const files = fs.readdirSync(path.resolve(__dirname, '../eventos/'))
-files.map(f => {
-	if (f.endsWith('.js')) client.on(f.slice(0, -3), require('../eventos/'+f).bind(null, client))
-})
+console.log('\n[Events]')
+const events = readdirSync(resolve(__dirname, '../eventos/'))
+const eventFilter = file => {
+	if (!file.endsWith('.js')) return
+	const path = resolve(__dirname, '..', 'eventos', file)
+	const filename = basename(path, '.js')
+	client.on(filename, require(path).bind(null, client))
+
+	console.log(`- ${filename} OK!`)
+}
+
+events.forEach(eventFilter)
 ////evt
 
 client.login(process.env.TOKEN)
